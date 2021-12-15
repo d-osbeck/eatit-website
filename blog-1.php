79e73956ea231f6258980784b44ecd5443d4e46a
David <david.osbeck1996@gmail.com>
2021-12-15 23:56:57 +0100

    Tilføjet undersider til blog opslag

---
+++ blog-1.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+    <title>Eat it - Gå ud af året med kold is og hot hud</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Eat it">
+    <meta name="copyright" content="© Eat it 2021. All Rights Reserved">
+    <meta name="keywords" content="">
+    <meta name="og:title" content="Gå ud af året med kold is og hot hud">
+    <meta name="og:description" content="Mange tænker – hjemmelavet is – det lyder besværligt, men det er det faktisk slet ikke.">
+    <meta name="og:type" content="article">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/blog.css" rel="stylesheet" type="text/css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body class="bg-primary">
+
+<header class="nav">
+    <div class="home__button__container container d-flex justify-content-center">
+        <a href="index.php">
+            <img src="img/logo-sort.svg" class="hero__logo py-5" alt="eatit logo">
+        </a>
+    </div>
+</header>
+
+<section class="blog__main">
+    <div class="blog__main__header container px-4">
+        <h1 class="blog__title display-2 fw-bold">Gå ud af året med kold is og hot hud!</h1>
+        <div class="blog__info__container d-flex align-items-center pt-4 pb-1">
+            <p class="blog__author fs-6">Camilla Hilm</p>
+            <div class="blog__dot__divider"></div>
+            <p class="blog__date fs-6">27. December 2021</p>
+        </div>
+        <div class="blog__line__divider"></div>
+    </div>
+
+    <div class="blog__main__content container px-4">
+        <img src="img/card-1.jpg" alt="hjemmelavet is" class="my-5">
+        <h1 class="blog__subtitle">Hjemmelavet is</h1>
+        <div class="blog__text fs-5 my-5">
+            <span class="fw-bold my-5">Tilberedningstid ca. 15 min.</span>
+        </div>
+        <li>2 stk æggeblommer </li><br>
+        <li>2 spsk flormelis</li> <br>
+        <li>1/2 stang vanilje</li> <br>
+        <li>2,5 dl piskefløde</li> <br>
+        <p class="blog__text fs-5 mb-5 mt-4">
+            man kan tilføre smag som lakridspulver, chokolade, frugt, marmelade og meget andet
+        </p>
+
+        <h1 class="blog__subtitle">Sådan gør du:</h1>
+        <p class="blog__text fs-5 my-5">
+            Tag 2 skåle frem: <br><br>
+            Skål 1: Pisk æg, flormelis og vanilje luftig <br><br>
+            Skål 2: Pisk fløden til flødeskum <br><br>
+            Herefter vendes skål 2 i skål 1, (ønsker man at tilføje smag, vendes dette forsigtigt i) massen puttes i en skål i fryseren med at stykke husholdningsfilm henover.
+            <br><br>
+            Har du ikke en ismaskine, så kan det let laves ca. 3 timer timer før dine gæster kommer og det giver bare lige det ekstra, at sige – så har jeg da lige lavet lidt hjemmelavet is til dessert.
+            <br><br>
+            Mens isen fryser, kan du lige så godt gøre lidt lækkert for dig selv. Sæt din yndlingsmusik på og nap et dejligt bad, skænk et glas vin, tænk på alle de fede ting det nye år vil bringe og læg bekymringerne på hylden for en stund og giv dig selv en lækker ansigtsmaske med blot en ingrediens og ingen kemi.
+        </p>
+        <h1 class="blog__subtitle">Ansigtsmaske med olivenolie</h1>
+        <p class="blog__text fs-5 my-5">
+            Olivenolie er fyldt med antioxidanter og anti-inflammatoriske stoffer. Olien er rigtig god til at forebygge rynker og hvem vil ikke gerne gå ind i det nye år med lækker hud?
+            <br>
+            <ul class="pb-4">Vask hænderne og dit ansigt grundigt med koldt vand. Dup huden tør med et håndklæde.</ul>
+            <ul class="pb-4">Tag lidt olivenolie i håndfladen og smør det direkte ind i ansigtet, på halsen og på brystkassen.</ul>
+            <ul class="pb-4">Skrub med en klud, til din hud føles glat og blød.</ul>
+            <ul class="pb-4">Skyl derefter af med vand.</ul>
+            <ul class="pb-4">Læg din make-up som sædvanligt.</ul>
+            <ul>Kig til isen</ul>
+        </p>
+
+        <p class="blog__text fs-5 my-5">
+            Så er der stadig rigeligt med tid til at lakere neglene og gøre kransekagen klar til kl. 00.00
+            <br><br>
+            Nyd tiden, nyd resten af året og ha´ det skønneste nytår – Vi ses her på bloggen i 2022
+        </p>
+
+        <h1 class="blog__subtitle py-5 px-3 text-center">Følg med på Facebook og Instagram og bliv opdateret hver gang der er nye indlæg på bloggen</h1>
+        <div class="blog__line__divider"></div>
+    </div>
+</section>
+
+<section class="footer">
+    <div class="footer__container container py-4">
+        <h3 class="footer__title p-4 px-md-5 mx-md-5 text-center">Download appen nu og lad os hjælpe dig igang</h3>
+
+        <div class="footer__buttons d-flex align-items-start flex-column align-items-center">
+            <a href="#" class="main-btn  my-2 d-flex align-items-center justify-content-center text-decoration-none">
+                <i class="fab fa-apple fa-lg me-2"></i>
+                <p class="">Download til iOS</p>
+            </a>
+            <a href="#" class="main-btn  my-2 d-flex align-items-center justify-content-center text-decoration-none">
+                <i class="fab fa-google-play me-2"></i>
+                <p class="">Download til Android</p>
+            </a>
+        </div>
+
+        <div class="footer__social d-flex justify-content-center mt-4 mb-5">
+            <a href="#" class="text-body text-decoration-none mx-2">
+                <img src="img/facebook.png" alt="instagram" class="social-icon">
+            </a>
+            <a href="#" class="text-body text-decoration-none mx-2">
+                <img src="img/instagram.png" alt="instagram" class="social-icon">
+            </a>
+        </div>
+
+        <div class="footer__copyright d-flex justify-content-center">
+            <p class="copyright__text">©Copyright Eatit 2021</p>
+        </div>
+    </div>
+</section>
+
+<script src="https://kit.fontawesome.com/d7a71e7a7e.js" crossorigin="anonymous"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
---
+++ blog-2.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+    <title>Eat it - Nytårsforsættet - det er svært at holde, men hjælpen er på vej!</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Eat it">
+    <meta name="copyright" content="© Eat it 2021. All Rights Reserved">
+    <meta name="keywords" content="">
+    <meta name="og:title" content="Nytårsforsættet">
+    <meta name="og:description" content="Det er svært at holde, men bare rolig, hjælpen er på vej!">
+    <meta name="og:type" content="article">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/blog.css" rel="stylesheet" type="text/css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body class="bg-primary">
+
+<header class="nav">
+    <div class="home__button__container container d-flex justify-content-center">
+        <a href="index.php">
+            <img src="img/logo-sort.svg" class="hero__logo py-5" alt="eatit logo">
+        </a>
+    </div>
+</header>
+
+<section class="blog__main">
+    <div class="blog__main__header container px-4">
+        <h1 class="blog__title display-2 fw-bold">Nytårsforsættet - det er svært at holde, men hjælpen er på vej!</h1>
+        <div class="blog__info__container d-flex align-items-center pt-4 pb-1">
+            <p class="blog__author">Camilla Hilm</p>
+            <div class="blog__dot__divider"></div>
+            <p class="blog__date">12. December 2021</p>
+        </div>
+        <div class="blog__line__divider"></div>
+    </div>
+
+    <div class="blog__main__content container px-4">
+        <img src="img/card-2.jpg" alt="spar penge" class="mt-5">
+        <p class="blog__text fs-5 my-5">
+            Vi kender begge 2 nytårsforsættet…. Den gode intention, indfaldet, det vi burde gøre… Det holder en uge og så er vi helt tilbage i den gamle rumle.
+            <br><br>
+            Det kan være alt fra lige at smide et par kilo, hjælpe folk i nød, gøre noget godt for miljøet, til at spare lidt penge og det er lige præcis på de sidste 2 punkter, den nye app kommer dig til undsætning.
+            <br><br>
+            Madspild er en kæmpe synder når det kommer til miljøet, men det er også en ret stor post, mange ikke tænker over i privatøkonomien.
+        </p>
+        <h1 class="blog__subtitle">Lidt kedelig statistik inden det bliver sjovt</h1>
+        <p class="blog__text fs-5 my-5">Iflg. Dansk statistik smider 40% af os rester af tilbehøret fra aftensmad ud mindst et par gange om måneden og 33 % af os smider kødrester ud ligeså.
+            <br><br>
+            En dansk husstand med 2 voksne og 2 børn bruger i gennemsnit 5159 kr om måneden på madposten* - Det er ca. 170 kr pr. dag.
+            <br><br>
+            Der er selvfølgelig forskel på familier, men lad os antage ca. 110 kr af dem går til aftensmad.
+            <br><br>
+            Lad os sige du bare en gang om ugen skifter et måltid lavet fra bunden ud, med et måltid lavet ud fra rester du ellers ville ha´ smidt ud og køber ingredienser for 30 kr.
+        </p>
+        <h1 class="blog__subtitle">Let the fun begin</h1>
+        <p class="blog__text fs-5 my-5">Så kan du spare 80 kr pr måltid og det lyder måske ikke af så meget, men læs lige videre!
+            <br><br>
+            Det svarer til 346 kr om måneden = ca. 1 klipning hos den lokale frisør.
+            <br><br>
+            Eller 4160 kr om året = ca. 26 biografbilletter inkl popcorn og sodavand (tag dit eget genanvendelige sugerør med i tasken, for de der papsugerør… De løfter altså ikke humøret).
+            <br><br>
+            Giver du den gas i 4 år, har du sparet 16.640 – og kan holde en skøn konfirmation for arvingen, en fantastisk rund fødselsdag eller en længe ventet ferie.
+            <br><br>
+            <span class="fw-bold"> Samtidig </span> har du og din familie undladt at smide op mod 752 kg spiselige madrester ud.
+            <br><br>
+            Det kan virke uoverskueligt i starten, men følg linket ind i Appstore eller Google Play og hent app´en ned og kom i gang med få klik.
+        </p>
+        <h1 class="blog__subtitle py-5 px-3 text-center">Følg med på Facebook og Instagram og bliv opdateret hver gang der er nye indlæg på bloggen</h1>
+        <div class="blog__line__divider"></div>
+    </div>
+</section>
+
+<section class="footer">
+    <div class="footer__container container py-4">
+        <h3 class="footer__title p-4 px-md-5 mx-md-5 text-center">Download appen nu, og begynd at spare på miljøet og din pengepung</h3>
+
+        <div class="footer__buttons d-flex align-items-start flex-column align-items-center">
+            <a href="#" class="main-btn  my-2 d-flex align-items-center justify-content-center text-decoration-none">
+                <i class="fab fa-apple fa-lg me-2"></i>
+                <p class="">Download til iOS</p>
+            </a>
+            <a href="#" class="main-btn  my-2 d-flex align-items-center justify-content-center text-decoration-none">
+                <i class="fab fa-google-play me-2"></i>
+                <p class="">Download til Android</p>
+            </a>
+        </div>
+
+        <div class="footer__social d-flex justify-content-center mt-4 mb-5">
+            <a href="#" class="text-body text-decoration-none mx-2">
+                <img src="img/facebook.png" alt="instagram" class="social-icon">
+            </a>
+            <a href="#" class="text-body text-decoration-none mx-2">
+                <img src="img/instagram.png" alt="instagram" class="social-icon">
+            </a>
+        </div>
+
+        <div class="footer__copyright d-flex justify-content-center">
+            <p class="copyright__text">©Copyright Eatit 2021</p>
+        </div>
+    </div>
+</section>
+
+    <script src="https://kit.fontawesome.com/d7a71e7a7e.js" crossorigin="anonymous"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+

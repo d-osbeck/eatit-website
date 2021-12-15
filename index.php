66176ca70f89b286e6cd39d0ce5772659e0d3e77
David <david.osbeck1996@gmail.com>
2021-12-15 23:56:32 +0100

    Forsiden er kodet og layout er færdigt, gjort responsiv til mobil, tablet og desktop

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+    <title>Eat it - Landing page</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Eat it">
+    <meta name="copyright" content="© Eat it 2021. All Rights Reserved">
+    <meta name="keywords" content="">
+    <meta name="og:title" content="Eat it - Kom hurtigt i gang">
+    <meta name="og:description" content="Eat it er app´en der inspirerer dig til at tænke i nye baner, stoppe madspild lidt efter lidt og gør at du altid har din indkøbsseddel lige ved hånden.">
+    <meta name="og:type" content="website">
+
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body class="bg-primary">
+
+<section class="hero">
+    <div class="hero__image text-hero-text d-lg-flex align-items-center justify-content-center pt-2 px-md-5 pt-xl-0">
+        <div class="hero__grid row g-0">
+            <div class="hero__container container d-flex justify-content-center align-items-center flex-column col-xl pt-lg-5">
+                <img src="img/logo-hvid.svg" class="hero__logo py-4 pt-lg-0" alt="eatit logo">
+                <h2 class="hero__subtitle px-4 pb-4 px-md-5 mx-md-5 text-center">Stop madspild og få idéer til aftensmaden</h2>
+
+                <div class="hero__buttons d-flex align-items-start flex-column align-items-center justify-content-center mt-4 mb-md-5 mx-md-5 flex-lg-row">
+                    <a href="#" class="main-btn my-2 d-flex align-items-center justify-content-center text-decoration-none mx-md-2">
+                        <i class="fab fa-apple fa-lg me-2"></i>
+                        <p class="">Download til iOS</p>
+                    </a>
+                    <a href="#" class="main-btn my-2 d-flex align-items-center justify-content-center text-decoration-none mx-md-2">
+                        <i class="fab fa-google-play me-2"></i>
+                        <p class="">Download til Android</p>
+                    </a>
+                </div>
+
+                <div class="hero__social d-flex justify-content-center mt-4 col-lg">
+                    <a href="#" class="text-hero-text text-decoration-none mx-2 mx-md-3">
+                        <i class="fab fa-facebook-square"></i>
+                    </a>
+                    <a href="#" class="text-hero-text text-decoration-none mx-2 mx-md-3">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div class="hero__mockup__container d-flex justify-content-center pt-4 col-lg">
+                <img src="img/mockup.png" class="phone__mockup h-auto" alt="iphone mockup app">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="info">
+    <div class="info__container container mb-5">
+        <div class="info__title__container d-flex justify-content-center flex-column">
+            <h1 class="info__title pb-3 px-4 px-md-5 mx-md-5 text-center">Så meget smider vi ud!</h1>
+            <p class="info__body px-4 px-md-5 mx-md-5 fs-5">I de danske husholdninger udgør madspild hvert år 247.000 ton, hvilket er mere end ⅓ af
+                den samlede mængde mad der i alt udgør 814.000 ton.
+                Det kan vi gøre meget bedre og samtidig hjælpe vores egen økonomi på vej.
+            </p>
+            <img src="img/infografik.png" alt="madspild infografik" class="py-5">
+        </div>
+    </div>
+    <svg class="info__wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#cfe0ca" fill-opacity="1" d="M0,224L24,186.7C48,149,96,75,144,80C192,85,240,171,288,170.7C336,171,384,85,432,42.7C480,0,528,0,576,42.7C624,85,672,171,720,197.3C768,224,816,192,864,192C912,192,960,224,1008,218.7C1056,213,1104,171,1152,176C1200,181,1248,235,1296,256C1344,277,1392,267,1416,261.3L1440,256L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path></svg>
+</section>
+
+<section class="main bg-secondary-bg pb-4">
+    <div class="main__koncept container py-4 py-md-5">
+        <h1 class="main__title pb-3 px-4 px-md-5 mx-md-5">Eat it - det nemme valg!</h1>
+        <p class="main__body px-4 px-md-5 mx-md-5 fs-5">Er mere end bare en app! Det er hjælp til dig, der gerne vil spare lidt på madbudgettet og samtidig gøre noget godt for miljøet.
+            Du indtaster blot de ingredienser du allerede har i hjemmet og straks kommer app´en med lækre og nemme løsninger.</p>
+    </div>
+
+    <div class="main__img my-4"></div>
+
+    <div class="main__about container pt-4 pb-5 py-md-5">
+        <h1 class="main__title pb-3 px-4 px-md-5 mx-md-5">Nemt, hurtigt og lige til</h1>
+        <p class="main__body px-4 px-md-5 mx-md-5 fs-5">Tjek køleskabet, skabe og fryseren og se hvilke nemme og lækre retter du kan fremtrylle hjemme i dit eget køkken.
+            Samtidig undgår du at fodre skraldespanden med penge, du i stedet kunne bruge på lidt lækkert til weekenden, tage en tur i biografen eller komme ud at rejse for.
+            De ingredienser du mangler, sender du med et klik videre til din indkøbsseddel, der gør det nemt for dig at købe de manglende ingredienser med hjem efter en travl dag.
+        </p>
+    </div>
+</section>
+
+<section class="blog">
+    <div class="blog__container container d-flex justify-content-center py-4 py-md-5">
+        <h1 class="main__title">Blog</h1>
+    </div>
+
+    <div class="blog__cards__container container d-flex flex-column flex-lg-row justify-content-center align-items-center mb-5">
+
+        <div class="blog__card w-100">
+            <a href="blog-1.php" class="text-decoration-none">
+                <div class="blog__card__wrapper card-1">
+                    <div class="blog__card__header d-flex justify-content-between p-4">
+                        <div class="blog__card__date">
+                            <p>27. December 2021</p>
+                        </div>
+                        <div class="blog__card__favorite">
+                            <div class="card__favorite__wrapper text-decoration-none text-hero-text"><p class="favorite-count">89</p><i class="fa fa-heart-o fa-lg"></i></div>
+                        </div>
+                    </div>
+
+                    <div class="blog__card__content p-4">
+                        <span class="blog__card__author fs-5">Camilla Hilm</span>
+                        <h1 class="blog__card__title pb-2">Gå ud af året med kold is og hot hud!</h1>
+
+                        <div class="blog__card__categories d-flex flex-wrap pb-3">
+                            <div class="category__item py-2 px-3 mt-1 me-1 me-lg-2">
+                                <p>Opskrift</p>
+                            </div>
+                            <div class="category__item py-2 px-3 mt-1 me-1 me-lg-2">
+                                <p>Dessert</p>
+                            </div>
+                            <div class="category__item py-2 px-3 mt-1 me-1 me-lg-2">
+                                <p>Gode tips</p>
+                            </div>
+                        </div>
+
+                        <div class="blog__card__content__wrapper">
+                            <p class="blog__card__text pb-4">Mange tænker – hjemmelavet is – det lyder besværligt, men det er det faktisk slet ikke.</p>
+                            <p class="text-center">Læs mere<i class="fas fa-arrow-right ms-1"></i></p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="blog__card w-100">
+            <a href="blog-2.php" class="text-decoration-none">
+                <div class="blog__card__wrapper card-2">
+                    <div class="blog__card__header d-flex justify-content-between p-4">
+                        <div class="blog__card__date">
+                            <p>12. December 2021</p>
+                        </div>
+                        <div class="blog__card__favorite">
+                            <div class="card__favorite__wrapper text-decoration-none text-hero-text"><p class="favorite-count">35</p><i class="fa fa-heart-o fa-lg"></i></div>
+                        </div>
+                    </div>
+
+                    <div class="blog__card__content p-4">
+                        <span class="blog__card__author fs-5">Camilla Hilm</span>
+                        <h1 class="blog__card__title pb-2">Nytårsforsættet</h1>
+
+                        <div class="blog__card__categories d-flex flex-wrap pb-3">
+                            <div class="category__item py-2 px-3 mt-1 me-1 me-lg-2">
+                                <p>Spar penge</p>
+                            </div>
+                            <div class="category__item py-2 px-3 mt-1 me-1 me-lg-2">
+                                <p>Råd</p>
+                            </div>
+                            <div class="category__item py-2 px-3 mt-1 me-1 me-lg-2">
+                                <p>Nytår</p>
+                            </div>
+                        </div>
+
+                        <div class="blog__card__content__wrapper">
+                            <p class="blog__card__text pb-4">Det er svært at holde, men bare rolig, hjælpen er på vej!</p>
+                            <p class="text-center">Læs mere<i class="fas fa-arrow-right ms-1"></i></p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
+
+<section class="footer">
+    <div class="footer__container container py-4">
+        <h3 class="footer__title p-4 px-md-5 mx-md-5 text-center">Download appen nu, og begynd at spare på miljøet og din pengepung</h3>
+
+        <div class="footer__buttons d-flex align-items-start flex-column align-items-center">
+            <a href="#" class="main-btn  my-2 d-flex align-items-center justify-content-center text-decoration-none">
+                <i class="fab fa-apple fa-lg me-2"></i>
+                <p class="">Download til iOS</p>
+            </a>
+            <a href="#" class="main-btn  my-2 d-flex align-items-center justify-content-center text-decoration-none">
+                <i class="fab fa-google-play me-2"></i>
+                <p class="">Download til Android</p>
+            </a>
+        </div>
+
+        <div class="footer__social d-flex justify-content-center mt-4 mb-5">
+            <a href="#" class="text-body text-decoration-none mx-2">
+                <img src="img/facebook.png" alt="instagram" class="social-icon">
+            </a>
+            <a href="#" class="text-body text-decoration-none mx-2">
+                <img src="img/instagram.png" alt="instagram" class="social-icon">
+            </a>
+        </div>
+
+        <div class="footer__copyright d-flex justify-content-center">
+            <p class="copyright__text">©Copyright Eatit 2021</p>
+        </div>
+    </div>
+
+</section>
+
+    <script src="https://kit.fontawesome.com/d7a71e7a7e.js" crossorigin="anonymous"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
